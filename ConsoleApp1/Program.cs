7280a8d50c944a7225f30df47372874968c56d8e
Giancarlos Salas <giansalex@gmail.com>
2017-09-15 22:40:14 -0500

    Initial Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using Ionic.Zip;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var zip = new ZipFile();
+            zip.AddEntry("paket.txt", "hello Paket");
+            zip.Save("test.zip");
+        }
+    }
+}
